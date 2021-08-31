af3adda878384f17b5628a36bc9c19404308c8ee
Gleysi Ascanio <gleysiascanio29@gmail.com>
2021-08-31 13:56:58 -0500

    Correcciones finales de pagina principal

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
-import { menu } from './lib/view/menu.js';
+import { home } from './lib/view/homepage.js';
 import { changeRoute } from './lib/router.js';
 
 const startApp = () => {
-  document.getElementById('root').innerHTML = menu();
+  document.getElementById('root').innerHTML = home();
   window.addEventListener('hashchange', () => {
     myFunction();
     changeRoute(window.location.hash);
