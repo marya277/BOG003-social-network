09169d7d7f7d6f371d3ea2688d0037881e2d7f61
Gleysi Ascanio <gleysiascanio29@gmail.com>
2021-09-07 20:55:54 -0500

    Validacion de Inputs de Formulario

--- src/lib/view/create.js
+++ src/lib/view/create.js
@@ -1,3 +1,5 @@
+import { validator } from '../index.js';
+
 export const create = () => {
   const divCreate = document.createElement('div');
   const viewCreate = `
@@ -14,27 +16,27 @@ export const create = () => {
 <form class="createAccount">
     <div class="input_create">
     <i class="fas fa-user"></i>
-        <input type="text" class="form__input" id="user_create" placeholder="Usuario">
+        <input type="text" class="form__input" id="user_create" placeholder="Usuario" required>
    </div>
 
     <div class="input_create">
       <i class="fas fa-envelope"></i>
-      <input type="text" class="form__input" id="email" placeholder="@Correo">
+      <input type="text" class="form__input" id="email" placeholder="@Correo" required>
     </div>
 
     <div class="input_create">
       <i class="fas fa-unlock-alt"></i>
-      <input type="text" class="form__input" id="password_create" placeholder="Contraseña">
+      <input type="text" class="form__input" id="password_create" placeholder="Contraseña" required>
     </div>
 
     <div class="input_create">
       <i class="fas fa-lock"></i>
-      <input type="text" class="form__input" id="pasword2_create" placeholder="Confirmar contraseña">
+      <input type="text" class="form__input" id="password2_create" placeholder="Confirmar contraseña" required>
     </div>
 
     <div class="form_send">
       <center>
-          <button type="submit" class="send">Enviar</button>
+          <button type="submit" class="send" id="">Enviar</button>
           <p class="form_confirmation_message" id="form_confirmation_message">Formulario enviado exitosamente!</p>
       </center>
   </form>
@@ -42,5 +44,32 @@ export const create = () => {
 <footer class="footerPrincipal"></footer>
 `;
   divCreate.innerHTML = viewCreate;
+  // Eventos de los Inputs --Usuario--Email--Contraseña
+  setTimeout(() => {
+    const email = document.getElementById('email');
+    email.addEventListener('change', validator);
+  }, 1000);
+
+  setTimeout(() => {
+    const userCreate = document.getElementById('user_create');
+    userCreate.addEventListener('change', validator);
+  }, 1000);
+
+  setTimeout(() => {
+    const passwordCreate = document.getElementById('password_create');
+    passwordCreate.addEventListener('change', validator);
+  }, 1000);
+
+  setTimeout(() => {
+    const passwordCreate2 = document.getElementById('password2_create');
+    passwordCreate2.addEventListener('change', validator);
+  }, 1000);
+
+  // setTimeout(() => {
+  // const send = document.getElementById('send');
+  // send.addEventListener('sumit', (e) => {
+  // e.preventDefault();
+  // });
+  // }, 1000);
   return divCreate;
 };
