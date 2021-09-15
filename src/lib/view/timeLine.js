223a02e2562bb8343277cf9e842f9a7828f79522
Gleysi Ascanio <gleysiascanio29@gmail.com>
2021-09-14 20:08:25 -0500

    Maquetación de vista timeLine, sección de publicar post

--- src/lib/view/timeLine.js
+++ src/lib/view/timeLine.js
@@ -2,21 +2,23 @@ export const timeLine = () => {
   const divTimeLine = document.createElement('div');
   const viewTimeLine = `
   <header>
-  <div class="headerPrincipal">
-  <img  class="imgTimeLine" src="img/Logo.png" alt="andariegos">
-  <a class="icon" href="#/timeLine"><i class="fas fa-home fa-2x" id="iconHome"></i></a>
-  <a class="icon" title="Salir" id="exit"><i class="fas fa-door-open fa-2x" id="iconDoor"></i></a>
-  <a class="icon" title="Perfil" href="#/profile"><i class="fas fa-user-circle fa-2x" id="iconProfile"></i></a>
-  </div>
+    <div class="headerPrincipal">
+      <img  class="imgTimeLine" src="img/Logo.png" alt="andariegos">
+      <a class="icon" href="#/timeLine"><i class="fas fa-home fa-2x" id="iconHome"></i></a>
+      <a class="icon" title="Salir" id="exit"><i class="fas fa-door-open fa-2x" id="iconDoor"></i></a>
+      <a class="icon" title="Perfil" href="#/profile"><i class="fas fa-user-circle fa-2x" id="iconProfile"></i></a>
+    </div>
   </header>
   <section class="containerTimeLine">
-  <div class="uno"></div>
-  <div class="dos"></div>
-  <div class="tres"></div>
-  <div class="cuatro"></div>
-  </section>
-  <div>
-  </div>`;
+    <div class="toPost" id="toPost">
+      <img id="photoProfile" src="img/iconProfile1.png">
+      <input id="publication" placeholder="¿Qué te gustaria compartir?"></input>
+      <button id="btnPost">Publicar</button>
+    </div>
+    <div class="dos"></div>
+    <div class="tres"></div>
+    <div class="cuatro"></div>
+  </section>`;
   divTimeLine.innerHTML = viewTimeLine;
 
   const exit = divTimeLine.querySelector('#exit');
